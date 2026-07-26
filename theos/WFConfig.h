e2334a6c0ea59980d0f322cccb84fa8cd253df7a
teeqp13-ops <teeqp13@gmail.com>
2026-07-26 20:46:23 +0300

    Connect WolFox GPS to unified activation API

--- theos/WFConfig.h
+++ theos/WFConfig.h
@@ -6,8 +6,10 @@
 #ifndef WFConfig_h
 #define WFConfig_h
 
-// عنوان واجهة التفعيل. لا تضع أي مفتاح سري داخل تطبيق iOS.
-#define WF_API_URL      @"https://gps.p3nd.fun/api/activate.php"
+// لوحة WolFox الموحدة. لا تضع أي مفتاح سري داخل تطبيق iOS.
+#define WF_API_URL      @"https://key.p3nd.fun/activation/api/v1/activate.php"
+#define WF_PROJECT_KEY  @"wolfox-gps"
+#define WF_BUNDLE_ID    @"com.wolfox.gpstweak"
 
 // مسار حفظ حالة التفعيل والإحداثيات
 #define WF_PREFS_PATH   @"/var/mobile/Library/Preferences/com.wolfox.gpstweak.plist"
